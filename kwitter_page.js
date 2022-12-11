50e26c793f2cb51fd059f51ae56e118dacd34cc3
Digvvijay <106813558+Digvvijay@users.noreply.github.com>
2022-12-11 11:04:21 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -27,7 +27,35 @@ function getData() { firebase.database().ref("/"+roomname).on('value', function(
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+console.log(firebase_message_id);
+console.log(message_data);
+name=message_data['name'];
+message=message_data['message'];
+like=message_data['like'];
+name_with_tag="<h4>"+name+"<img class='user_tick' src='tick.png'> </h4>";
+message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
+like_button="<button id='+firebase_message_id+' type='button' class='btn btn-warning' onclick='updatelike(this.id)' value="+like+">";
+span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>Like:"+like+"</span></button><hr>";
+row=name_with_tag+message_with_tag+like_button+span_with_tag;
+document.getElementById("output").innerHTML+=row;
 //End code
       } });  }); }
 getData();
+function updatelike(message_id)
+{
+      console.log("clicked on the button-"+message_id);
+      button_id=message_id;
+      likes=document.getElementById(button_id).value;
+      updatedlikes=Number(likes)+1;
+      console.log(updatedlikes);
+      firebase.database().ref(roomname).child(message_id).update({
+            like:updatedlikes
+      });
+}
+
+function logout()
+{
+      localStorage.removeItem("roomname");
+      localStorage.removeItem("username");
+      window.location="index.html";
+}
